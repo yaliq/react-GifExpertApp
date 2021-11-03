628940b703c0cc36e9246609d58fe2e5e46f718b
yaliq <yamur.aliquintui@gmail.com>
2021-11-03 00:23:21 -0300

    corrección de una importación

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
+//import { useEffect } from "react/cjs/react.development";
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = (category) => {
